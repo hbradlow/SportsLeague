6e689647932ab499dcb74342d0448e9081cf3e6a
Naren Sathiya <Naren Sathiya nsathiya@spawnd.com>
2012-08-22 15:34:24 -0700

    Added links, changed nav to twitter class, error with Reverse match

--- sports/sports/apps/sports_tracking/urls.py
+++ sports/sports/apps/sports_tracking/urls.py
@@ -7,4 +7,10 @@ from sports_tracking.views import *
 
 urlpatterns = patterns('sports_tracking.views',
     url(r'^$', home,name="home"),
+    url(r'^flag_football$', flag_football, name="flag_football"),
+    url(r'^volleyball/$', volleyball, name="volleyball"),
+    url(r'^basketball/$', basketball, name="basketball"),
+    url(r'^soccer/$', soccer, name="soccer"),
+    url(r'^softball/$', softball, name="softball"),
+    url(r'^hockey/$', hockey, name="hockey"),
 )
--- sports/sports/apps/sports_tracking/views.py
+++ sports/sports/apps/sports_tracking/views.py
@@ -12,3 +12,21 @@ def home(request):
     sport_HY = Sport.objects.get(type="HY").display()
     fraternities_list = Fraternity.objects.all()
     return render_to_response("sports_tracking/home.html",{"sport_FF": sport_FF, "sport_VB": sport_VB, "sport_BB": sport_BB, "sport_SR": sport_SR, "sport_SB": sport_SB, "sport_HY": sport_HY, "fraternities_list": fraternities_list},)
+
+def flag_football(request):
+    return render_to_response("sports_tracking/flag_football.html")
+
+def volleyball(request):
+    return render_to_response("sports_tracking/volleyball.html")
+
+def basketball(request):
+    return render_to_response("sports_tracking/basketball.html")
+
+def soccer(request):
+    return render_to_response("sports_tracking/soccer.html")
+
+def softball(request):
+    return render_to_response("sports_tracking/softball.html")
+
+def hockey(request):
+    return render_to_response("sports_tracking/hockey.html")
